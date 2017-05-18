2b5224ebbbb0ef0cb8089f02548525ef9da3ce97
Iker <Iker@192.168.1.39>
2017-05-18 17:36:07 +0200

    Pedido Modelo terminado.

--- src/es/starbooks/modelo/PedidoModelo.java
+++ src/es/starbooks/modelo/PedidoModelo.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-
+import es.starbooks.clase.Libro;
 import es.starbooks.clase.Pedido;
 import es.starbooks.conector.Conector;
 
@@ -15,7 +15,7 @@ public class PedidoModelo extends Conector{
 	public ArrayList<Pedido> seleccionarPorId_libro(int id_libro){
 		
 		ArrayList<Pedido> pedidos = new ArrayList<Pedido>();
-		
+		ArrayList<Libro> libros = new ArrayList<Libro>();
 				
 		try {
 			
@@ -23,29 +23,30 @@ public class PedidoModelo extends Conector{
 			ResultSet rs = st.executeQuery("SELECT p.id_usuario, l.titulo, l.autor, l.editorial, l.cantidad FROM pedido p JOIN libro l ON (p.id_libro=l.id_libro) WHERE id_libro = '" + id_libro + "'");
 			
 			while(rs.next()){
-													
+				
+				Libro libro = new Libro();
+				libro.setTitulo(rs.getString("titulo"));
+				libro.setAutor(rs.getString("autor"));
+				libro.setEditorial(rs.getString("editorial"));
+				libro.setCantidad(rs.getInt("cantidad"));
+				
+				libros.add(libro);
+								
 				Pedido pedido = new Pedido();
 				pedido.setId_usuario(rs.getInt("id_usuario"));
+				pedido.setLibros(libros);
 				
+				pedidos.add(pedido);
 				
-			
-			
-				
-				
+				return pedidos;
 				
 			}
-			
-
-			
-			
-			
-			
-			
-			
+					
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 		return null;
 		
 		
